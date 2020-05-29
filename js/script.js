917e2fa160e2dbcab198d6dc059baf93caa759ec
Sara <saracorsob@gmail.com>
2020-05-29 17:21:57 +0200

    Correzione funzione : eliminazione ciclo + validazione

--- js/script.js
+++ js/script.js
@@ -4,28 +4,31 @@
 
 // Chiedo all'utente una parola
 var parolaUtente = prompt('Inserisci una parola');
-
+while ( !(isNaN(parolaUtente)) || parolaUtente == '' ) {
+  parolaUtente = prompt('Errore. Inserisci una parola');
+}
 
 // Creo una funzione per capire se la parola è palindroma
 function parolaPalindroma(argParolaUtente) {
-// Dichiaro array vuoto per la parola inserita
+  // Dichiaro array vuoto per la parola inserita
   var arrayParolaUtente = [];
-// Standardizzazione parola inserita
+
+  // Standardizzazione parola inserita
   var argParolaUtenteMin = argParolaUtente.toLowerCase();
 
-// Ciclo per riempire l'array
+  // Ciclo per riempire l'array
   for (var i = 0; i < argParolaUtenteMin.length; i++) {
     arrayParolaUtente.push(argParolaUtenteMin[i]);
   }
-// Variabile con l'array al contrario
-  var parolaPalindroma = arrayParolaUtente.reverse();
 
-// Ciclo per comparare i due array e capire se sia palindroma o no
+  // Array al contrario e non trasformate in stringhe per la comparazione
+  var stringParolaUtente = arrayParolaUtente.toString();
+  var parolaPalindroma = (arrayParolaUtente.reverse()).toString();
+
+  // Ciclo per comparare le due stringhe e capire se sia palindroma o no
   var risultato = false;
-  for (var i = 0; i < parolaPalindroma.length; i++) {
-    if (parolaPalindroma[i] === argParolaUtenteMin[i]) {
-      risultato = true;
-    }
+  if (parolaPalindroma === stringParolaUtente) {
+    risultato = true;
   }
 
 // Risultato finale
@@ -36,6 +39,7 @@ return risultato;
 // Stampo la risposta
 var risultatoStampato = document.getElementById('titolo');
 var messaggio = 'La parola  "' +  parolaUtente + '" non è palindroma';
+
 if (parolaPalindroma(parolaUtente) === true) {
   messaggio = 'La parola: ' + parolaUtente + ' è palindroma';
 }
