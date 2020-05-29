e4edc237c641fc6f5a51fd8e4d1b2ed168b37784
Sara <saracorsob@gmail.com>
2020-05-29 16:42:14 +0200

    Modifica a stampa e aggiunta valore booleano

--- js/script.js
+++ js/script.js
@@ -8,23 +8,36 @@ var parolaUtente = prompt('Inserisci una parola');
 
 // Creo una funzione per capire se la parola è palindroma
 function parolaPalindroma(argParolaUtente) {
+// Dichiaro array vuoto per la parola inserita
   var arrayParolaUtente = [];
+// Standardizzazione parola inserita
   var argParolaUtenteMin = argParolaUtente.toLowerCase();
+
+// Ciclo per riempire l'array
   for (var i = 0; i < argParolaUtenteMin.length; i++) {
     arrayParolaUtente.push(argParolaUtenteMin[i]);
   }
+// Variabile con l'array al contrario
   var parolaPalindroma = arrayParolaUtente.reverse();
 
+// Ciclo per comparare i due array e capire se sia palindroma o no
+  var risultato = false;
   for (var i = 0; i < parolaPalindroma.length; i++) {
-    var risultato = ' non è palindroma';
     if (parolaPalindroma[i] === argParolaUtenteMin[i]) {
-      var risultato = ' é palindroma';
+      risultato = true;
     }
   }
 
+// Risultato finale
 return risultato;
 
 }
 
 // Stampo la risposta
-document.getElementById('titolo').innerHTML = 'La parola ' + parolaUtente + parolaPalindroma(parolaUtente);
+var risultatoStampato = document.getElementById('titolo');
+var messaggio = 'La parola  "' +  parolaUtente + '" non è palindroma';
+if (parolaPalindroma(parolaUtente) === true) {
+  messaggio = 'La parola: ' + parolaUtente + ' è palindroma';
+}
+
+risultatoStampato.innerHTML = messaggio;
